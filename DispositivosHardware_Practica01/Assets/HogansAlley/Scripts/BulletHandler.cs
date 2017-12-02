cc40103df4bf1760d6a8d4967a4bcc6f1abf63d4
LIVEU-TAD\eugenio.salguero <essalguero@yahoo.es>
2017-12-02 10:44:56 +0100

    no message

--- DispositivosHardware_Practica01/Assets/HogansAlley/Scripts/BulletHandler.cs
+++ DispositivosHardware_Practica01/Assets/HogansAlley/Scripts/BulletHandler.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class BulletHandler : MonoBehaviour {
 
     private Rigidbody rb;
+    private AudioSource audioSource;
+    public AudioClip audioClipHit;
     public ParticleSystem ps;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent < Rigidbody >();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,11 +22,13 @@ public class BulletHandler : MonoBehaviour {
 
     public void Shoot(Vector3 direction)
     {
-
+        //Empujar la pelota
+        rb.AddForce(direction, ForceMode.Impulse);
     }
 
     public void onCollisionEnter(Collider other)
     {
-
+        ps.Play();
+        audioSource.PlayOneShot(audioClipHit);
     }
 }
