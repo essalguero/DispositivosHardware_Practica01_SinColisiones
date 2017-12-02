4d109b9b4b77f71e7f0a3c4b5d157b08e8e6dfe4
LIVEU-TAD\eugenio.salguero <essalguero@yahoo.es>
2017-12-02 10:28:07 +0100

    no message

---
+++ DispositivosHardware_Practica01/Assets/HogansAlley/Scripts/BulletHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHandler : MonoBehaviour {
+
+    private Rigidbody rb;
+    public ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent < Rigidbody >();
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+		
+	}*/
+
+    public void Shoot(Vector3 direction)
+    {
+
+    }
+
+    public void onCollisionEnter(Collider other)
+    {
+
+    }
+}
